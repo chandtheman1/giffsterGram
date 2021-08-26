8c8f1771ef19ae88870c3f9bb1fa6e25b1dad712
Jesse De Jong <jdoceau@gmail.com>
2021-08-26 20:44:43 +1000

    Wrote adding text to gif funciton, added dependencies to package.json for JIMP and GIFWRAP, wrote temp API route for calling function

---
+++ controllers/api/addTextToGif.js
@@ -0,0 +1,32 @@
+const Jimp = require('jimp');
+const { GifUtil, GifCodec, BitmapImage, GifFrame } = require('gifwrap');
+
+// Async processing function for adding text to GIFs
+async function addTextToGIF() { 
+    // Get source path of GIF from the user
+    const sourceGif = "input-img/what-is-love.gif"; // Currently hardcoded
+    // Read the file at the path and create a [GIFWRAP] Bitmap array
+    const inputGif = await GifUtil.read(sourceGif);
+    // Create an array of the frames subobject
+    const frames = inputGif.frames.map((item) => item);
+    // Convert the array of [GIFWRAP] frames to [JIMP] frames
+    const jimpFrames = frames.map((item) => {
+        const frame = GifUtil.copyAsJimp(Jimp, item);
+        return frame });
+    // Load the [.fnt] font file for adding the text overlay 
+    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    // Get source text from the user
+    const sourceText = 'Hello World!'; // Currently hardcoded
+    // Overlay the user input text to each frame of the gif in the [JIMP] array
+    const jimpTextFrames = jimpFrames.map((item) => {
+        const textFrame = item.print(font, 10, 10, sourceText);
+        return textFrame });
+    // Convert the array of [JIMP] frames to [GIFWRAP] frames
+    const gifWrapTextFrames = jimpTextFrames.map((item) => {
+        const fCopied = new GifFrame(new BitmapImage(item.bitmap))
+        return fCopied });
+    // Write the amended GIF to the destination path
+    GifUtil.write("images/test-gifwrap.gif", gifWrapTextFrames);
+};
+
+module.exports = { addTextToGIF }
