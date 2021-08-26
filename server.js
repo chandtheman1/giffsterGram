d18c37620d27f084d44215b90e59f7ead34cb80b
Chandler Liang <chandler.liang123@gmail.com>
2021-08-26 19:34:40 +1000

    fixed the server port

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
     res.send('Hello World')
   });
    
-app.listen(`App is listening on ${PORT}`);
+app.listen(PORT);
