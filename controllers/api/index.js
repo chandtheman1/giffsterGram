8518f15ca07f90477b1c2c9c26e2627404a2157e
Mayuri Pendyala <mayu04@gmail.com>
2021-08-30 21:59:27 +1000

    added  user and comment routes

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const gifRoutes = require('./gif-routes.js');
+const commentRoutes = require('./comment-routes.js');
+const tagRoutes = require('./tag-routes.js');
+const threadRoutes = require('./thread-routes.js');
+const userRoutes = require('./user-routes.js');
+const imageProcessing = require('./imageProcessing');
+
+router.use('/gif', gifRoutes);
+router.use('/comment', commentRoutes);
+router.use('/tag', tagRoutes);
+router.use('/thread', threadRoutes);
+router.use('/user', userRoutes);
+router.use('/image', imageProcessing);
+
+module.exports = router;
---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// middleware  to verify  user logged in before restricted route access
+
+const withAuth =(req, res, next) => {
+
+    if(!req.session.userId) {
+        res.redirect('/login');
+    }else{
+        next();
+    }
+};
+
+module.exports = withAuth;
