2aa4b41e5a5f996043d43007d20236e70bc043a3
Jesse De Jong <jdoceau@gmail.com>
2021-08-31 20:37:26 +1000

    Fixed server issues with index.js in routing folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,20 +1,15 @@
 const router = require('express').Router();
+
 const gifRoutes = require('./gif-routes.js');
-const commentRoutes = require('./comment-routes.js');
 const tagRoutes = require('./tag-routes.js');
-const threadRoutes = require('./thread-routes.js');
 const userRoutes = require('./user-routes.js');
 const imageProcessing = require('./imageProcessing');
-const gifPathStorage = require('./gifRoutes');
-const tagRoutes = require('./tagRoutes');
+const commentRoutes = require('./comment-routes.js');
 
+router.use('/tag', tagRoutes);
 router.use('/gif', gifRoutes);
-router.use('/comment', commentRoutes);
-router.use('/tag', tagRoutes);
-router.use('/thread', threadRoutes);
 router.use('/user', userRoutes);
+router.use('/comment', commentRoutes);
 router.use('/image', imageProcessing);
-router.use('/gif', gifPathStorage);
-router.use('/tag', tagRoutes);
 
 module.exports = router;
