46536a89149d49533ff50550e8b970b0a1c36795
Mayuri Pendyala <mayu04@gmail.com>
2021-09-05 22:13:16 +1000

    fixed login fail issue

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,23 +1,31 @@
-const signupFormHandler =  function(event) {
+const signupFormHandler = async function(event) {
     event.preventDefault();
   
-    const usernameEl = document.querySelector("#username-input-signup");
-    const passwordEl = document.querySelector("#password-input-signup");
-    const emailEl = document.querySelector("#email-input-signup");
-    console.log(usernameEl.value,passwordEl.value,emailEl.value);
-    fetch("/api/user", {
-      method: "post",
-      body: JSON.stringify({
-        username: usernameEl.value,
-        password: passwordEl.value,
-        email: emailEl.value,
-      }),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(function() {
-        document.location.replace("/gifs"); // <------ needs to be updated
-      })
-      .catch(err => console.log(err));
+    const username = document.querySelector("#username-input-signup").value.trim();
+    const password = document.querySelector("#password-input-signup").value.trim();
+    const email = document.querySelector("#email-input-signup").value.trim();
+    console.log(username,password,email);
+
+    if(username && password && email){
+
+      const response = await fetch("/api/user", {
+        method: "post",
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          email: email,
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+
+        if(response.ok) {
+          // If successful, redirect the browser to the gifs page
+          document.location.replace('/gifs');
+        } else {
+          alert(response.statusText);
+        }
+    }
+  
   };
   
   // When user clicks login redirect to the login options
