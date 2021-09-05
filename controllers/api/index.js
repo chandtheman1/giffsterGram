28b6e6a08f6d0352ab4056eab591748121d843cf
Chandler Liang <chandler.liang123@gmail.com>
2021-09-05 21:26:18 +1000

    reorganise folder structure

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 
 const gifRoutes = require('./gif-routes.js');
 const userRoutes = require('./user-routes.js');
-const imageProcessing = require('./imageProcessing');
+// const imageProcessing = require('./imageProcessing');
 const commentRoutes = require('./comment-routes.js');
 const uploadRoutes = require('./upload-routes');
 
 router.use('/gif', gifRoutes);
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
-router.use('/image', imageProcessing);
+// router.use('/image', imageProcessing);
 router.use('/upload', uploadRoutes);
 
 module.exports = router;
